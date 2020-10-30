e8e4bef439bae52d350893daf5f9774afa2b2a82
Zhouhao Rui <976667412@qq.com>
2020-10-30 23:29:26 +0000

    Added Upcoming movies page

--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -1,12 +1,14 @@
 import React, { memo } from 'react'
 
 export default memo(function Header({ numMovies }) {
+  console.log(window.location.pathname)
   return (
     <div>
       <div className="row">
         <div className="col-md-6 offset-4">
           <h2>
-            No. Movies{" "}
+            {window.location.pathname === '/movies/upcoming' ? 
+             "Upcoming Movies " : "All Movies "}
             <span className="badge badge-pill badge-success">{numMovies}</span>
           </h2>
         </div>
---
+++ src/pages/upcomingMoviesPage.js
@@ -0,0 +1,33 @@
+import React, {memo, useEffect, useState} from 'react'
+import PageTemplate from "../components/templateMovieListPage";
+import StubAPI from '../api/stubAPI'
+import {getUpcomingMovies} from '../api/tmdb-api'
+
+const UpcomingMoviesPage = () => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    getUpcomingMovies().then(res => {
+      setMovies(res)
+    })
+  }, [])
+
+  const addToFavorites = movieId => {
+    setMovies(movies => {
+      const index = movies.map(m => m.id).indexOf(movieId);
+      StubAPI.add(movies[index]);
+      let newMoviesState = [...movies]
+      newMoviesState.splice(index, 1);
+      return newMoviesState;
+    });
+  }
+
+  return (
+    <PageTemplate
+        title='Discover Movies'
+        movies={movies}
+        buttonHandler={addToFavorites}
+      />
+  )
+}
+
+export default memo(UpcomingMoviesPage)
