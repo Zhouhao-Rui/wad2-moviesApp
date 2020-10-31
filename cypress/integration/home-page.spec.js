b2f87460110f24c02da44c563c4e10cc84c68c26
Zhouhao Rui <976667412@qq.com>
2020-10-31 00:20:00 +0000

    E2E tests - Home page - Filtering by title

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,7 +1,29 @@
+let movies;
+
+const filterByTitle = (movieList, string) => {
+  return movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+}
+
+const filterByGenre = (movieList, genreId) => {
+  return movieList.filter((m) => m.genre_ids.includes(genreId));
+}
+
 describe('Home Page', () => {
-  beforeEach(() => {
-    cy.visit('/');
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")   
+      .then((response) => {
+        movies = response.results
+      })
   })
+
+  beforeEach(() => {
+    cy.visit('/')
+  });
 
   describe('Base Test', () => {
     it('displays page header', () => {
@@ -9,6 +31,36 @@ describe('Home Page', () => {
       cy.get('.badge').contains(20);
     })
   })
+
+  describe("Filtering", () => {
+    describe("By movie title" ,() => {
+      it("should display movies with 'p ' in the title", () => {
+        const searchString = 'p'
+        const matchingMovies = filterByTitle(movies, searchString );
+        cy.get("input").clear().type(searchString) ;
+        cy.get(".card").should("have.length", matchingMovies.length);
+        cy.get(".card").each(($card, index) => {
+          cy.wrap($card)
+          .find(".card-title")
+          .should("have.text", matchingMovies[index].title);
+        });
+      })
+      it("should display movies with 'o' in the title", () => {
+        const searchString = "o";
+        const matchingMovies = filterByTitle(movies, searchString);
+        cy.get("input").clear().type(searchString);
+        cy.get(".card").should("have.length", matchingMovies.length);
+        cy.get(".card").each(($card, index) => {
+          cy.wrap($card)
+          .find(".card-title")
+          .should("have.text", matchingMovies[index].title);
+        })
+      })
+    })
+    describe("By movie genre" ,() => {
+      // More later
+    })
+  });
 })
 
 
