5b2b6baf6141af9503dfdf946bd8e32afdab959f
Zhouhao Rui <976667412@qq.com>
2020-11-18 11:10:59 +0000

    Firebase: Refactor the environment variables

---
+++ src/contexts/authContext.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../config/firebase'
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState("")
+  const [loading, setLoading] = useState(true)
+
+  const signup = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  useEffect(() => {
+    // callback function to unsubscribe the onAuthStateChange event
+    const unsubsciber = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    // onMount
+    return unsubsciber
+  }, [])
+
+  const value = {
+    currentUser,
+    signup
+  }
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
