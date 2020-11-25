2da23e2e18660151d9b547fc44040ad8b1e4f6ea
Zhouhao Rui <976667412@qq.com>
2020-11-25 12:34:25 +0000

    Floating Button: Add Floating button component basic style and structure

---
+++ src/components/floatingButton/childrenComponents/WatchLists.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function WatchLists() {
+  return (
+    <>
+      <div className="circle circle1">All</div>
+    </>
+  )
+}
+
+export default WatchLists
