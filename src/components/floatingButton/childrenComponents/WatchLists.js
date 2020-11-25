1094661fc200465734f316c3dfdcc5ad4305585c
Zhouhao Rui <976667412@qq.com>
2020-11-25 15:34:02 +0000

    Floating Button: Add movie circle button when clicking floating button

--- src/components/floatingButton/childrenComponents/WatchLists.js
+++ src/components/floatingButton/childrenComponents/WatchLists.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './style.css'
 
-function WatchLists() {
+function WatchLists({ watchLists }) {
   return (
     <>
-      <div className="circle circle1">All</div>
+      <div className="circle circle1 text-white">All</div>
+      {
+        watchLists.length === 1 ?
+        <div className="circle circle2" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${watchLists[0].poster_path})`}}></div>
+        :
+        <>
+        <div className="circle circle2" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${watchLists[0].poster_path})`}}></div>
+        <div className="circle circle3" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${watchLists[1].poster_path})`}}></div>
+        </>
+      }
     </>
   )
 }
