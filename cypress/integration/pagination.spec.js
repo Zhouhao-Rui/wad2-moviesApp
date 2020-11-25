3ba2b3109a824f8d2dd39a37827698652c4868d0
Zhouhao Rui <976667412@qq.com>
2020-11-25 22:41:06 +0000

    Pagination test: Add test for upcoming pagination

--- cypress/integration/pagination.spec.js
+++ cypress/integration/pagination.spec.js
@@ -52,4 +52,55 @@ describe('Pagination test', () => {
     })
   })
   
+  describe('Upcoming page pagination test', () => {
+    beforeEach(() => {
+      cy.visit('/movies/upcoming')
+    })
+    it('should still show the current page after clicking the current page link', () => {
+      cy.get('[data-cy=page-active]').click()
+      cy.get('.card-body').find(`.card-title:contains(${upcomingMovieName})`).should('have.length', 1)
+    })
+    it('should show the specific page after clicking the specific page link', () => {
+      cy.get('.page-link').eq(2).click()
+      cy.wait(2000)
+      cy.get('.page-item').eq(2).each($el => {
+        expect($el).to.have.class('active')
+      })
+      cy.get('.card-body').each($el => {
+        cy.wrap($el).find('.card-title').should('not.contain', upcomingMovieName)
+      })
+    });
+    it('should show the next page data after clicking the next link', () => {
+      cy.get('[data-cy=next-link]').click()
+      cy.wait(2000)
+      cy.get('.card-body').each($el => {
+        cy.wrap($el).find('.card-title').should('not.contain', upcomingMovieName)
+      })
+    })
+    it('should show the previous page data after clicking the previous link', () => {
+      cy.get('[data-cy=next-link]').click()
+      cy.wait(2000)
+      cy.get('[data-cy=pre-link]').click()
+      cy.wait(2000)
+      cy.get('.card-body').find(`.card-title:contains(${upcomingMovieName})`).should('have.length', 1)
+    })
+    it('should support add watch list movies from different page and the current page keep alive in the website', () => {
+      cy.get('.btn').eq(0).click()
+      cy.get('.btn').eq(1).click()
+      cy.get('[data-cy=next-link]').click()
+      cy.wait(2000)
+      cy.get('.btn').eq(0).click()
+      cy.get('.btn').eq(1).click()
+      cy.get('[data-cy=floating-window]').click()
+      cy.get('.circle1').click()
+      cy.wait(2000)
+      cy.url().should('include', 'watchLists')
+      cy.get('.card').should('have.length', 4)
+      cy.get('.nav-link').eq(1).click()
+      cy.get('.page-item').eq(1).each($el => {
+        expect($el).to.have.class('active')
+      })
+      cy.get('.card').should('have.length', 18)
+    })
+  })
 })
