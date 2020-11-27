412cd7e245b7f859f594bf54201ed30312c67322
Zhouhao Rui <976667412@qq.com>
2020-11-27 21:09:22 +0000

    Side bar: Add basic side bar in the site header component

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -4,16 +4,134 @@ import "../../globals/fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./siteHeader.css";
 
+/** Material UI */
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MovieIcon from '@material-ui/icons/Movie';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import Input from '@material-ui/core/Input';
+import SearchIcon from '@material-ui/icons/Search';
 import { useAuth } from '../../contexts/authContext'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+}));
 
 const SiteHeader = ({ history }) => {
   const { signout, currentUser } = useAuth()
+
+  /** Material UI */
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <nav className="navbar fixed-top navbar-light bg-dark">
       <nav className="navbar-brand text-white">
-        <Link className="text-white" to="/">
-          TMDB Client
-        </Link>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))} */}
+            <ListItem>
+              <ListItemIcon>
+                <SearchIcon />
+              </ListItemIcon>
+              <Input />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <MovieIcon />
+              </ListItemIcon>
+              <ListItemText primary="Movie" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <LiveTvIcon />
+              </ListItemIcon>
+              <ListItemText primary="TV" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <VideoLibraryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Collection list" />
+            </ListItem>
+          </List>
+        </Drawer>
       </nav>
       <FontAwesomeIcon
         className="navbar-text text-light"
@@ -62,18 +180,18 @@ const SiteHeader = ({ history }) => {
             if user is authorized, it will show the logout and profile link
           */}
           {currentUser &&
-          <>
-            <li>
-              <Link className="nav-link text-white" to="/profile">
-                profile
+            <>
+              <li>
+                <Link className="nav-link text-white" to="/profile">
+                  profile
               </Link>
-            </li>
-            <li className="nav-item">
-              <p className="nav-link text-white" onClick={() => {signout(); history.go(0)}} style={{ cursor: "pointer" }}>
-                logOut
+              </li>
+              <li className="nav-item">
+                <p className="nav-link text-white" onClick={() => { signout(); history.go(0) }} style={{ cursor: "pointer" }}>
+                  logOut
             </p>
-            </li>
-          </>
+              </li>
+            </>
           }
         </ul>
       </nav>
